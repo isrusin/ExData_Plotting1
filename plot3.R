f0eff3bfc00a2238f5b249a972bf253b4c89b456
Ivan Rusinov <is_rusinov@mail.ru>
2015-04-10 14:13:01 +0300

    set background transparent and make some syntax improvements

--- plot3.R
+++ plot3.R
@@ -1,22 +1,23 @@
-## The script loads household_power_consumption.txt, 
-## selects Sub_metering values from the dates 2007-02-01 and 2007-02-02, 
+## The script loads household_power_consumption.txt,
+## selects Sub_metering values from the dates 2007-02-01 and 2007-02-02,
 ## and plots these values vs. time.
 
 library("data.table")
-data <- fread("household_power_consumption.txt", header = T, stringsAsFactors = F, 
+data <- fread("household_power_consumption.txt", header = T, stringsAsFactors = F,
               colClasses = list(character = 1:9), sep = ";", na.strings = "?")
-vals <- data[Date %in% c("1/2/2007", "2/2/2007"), 
-             list("Time" = strptime(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"), 
-                  "Sub1" = as.numeric(Sub_metering_1), 
-                  "Sub2" = as.numeric(Sub_metering_2), 
+vals <- data[Date %in% c("1/2/2007", "2/2/2007"),
+             list("Time" = strptime(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"),
+                  "Sub1" = as.numeric(Sub_metering_1),
+                  "Sub2" = as.numeric(Sub_metering_2),
                   "Sub3" = as.numeric(Sub_metering_3))]
 Sys.setlocale("LC_TIME", "en_US.UTF-8")
-png(file="plot3.png")
-plot(vals$Time, vals$Sub1, ylab = "Energy sub metering", type = "n", main = "", 
-     xlab = "")
-lines(vals$Time, vals$Sub1, col = "black")
-lines(vals$Time, vals$Sub2, col = "red")
-lines(vals$Time, vals$Sub3, col = "blue")
-legend("topright", lty = c(1, 1, 1), col = c("black", "red", "blue"),
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+png(file = "plot3.png", bg = "transparent")
+with(vals, {
+	plot(Time, Sub1, xlab = "", ylab = "Energy sub metering", type = "n")
+	lines(Time, Sub1, col = "black")
+	lines(Time, Sub2, col = "red")
+	lines(Time, Sub3, col = "blue")
+	legend("topright", lty = c(1, 1, 1), col = c("black", "red", "blue"),
+	       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+})
 dev.off()
--- plot4.R
+++ plot4.R
@@ -15,19 +15,23 @@ vals <- data[Date %in% c("1/2/2007", "2/2/2007"),
                   "Sub2" = as.numeric(Sub_metering_2),
                   "Sub3" = as.numeric(Sub_metering_3))]
 Sys.setlocale("LC_TIME", "en_US.UTF-8")
-png(file="plot4.png")
+png(file = "plot4.png", bg = "transparent")
 par(mfrow=c(2,2))
 with(vals, {
-	plot(Time, GAP, ylab = "Global Active Power", type = "n", xlab = "")
+	# top left plot
+	plot(Time, GAP, xlab = "", ylab = "Global Active Power", type = "n")
 	lines(Time, GAP, col = "black")
+	# top right plot
 	plot(Time, Voltage, xlab = "datetime", ylab = "Voltage", type = "n")
 	lines(Time, Voltage, col = "black")
-	plot(Time, Sub1, ylab = "Energy sub metering", type = "n", xlab = "")
+	# bottom left plot
+	plot(Time, Sub1, xlab = "", ylab = "Energy sub metering", type = "n")
 	lines(Time, Sub1, col = "black")
 	lines(Time, Sub2, col = "red")
 	lines(Time, Sub3, col = "blue")
 	legend("topright", lty = c(1, 1, 1), bty = "n", col = c("black", "red", "blue"),
 	       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+	# bottom right plot
 	plot(Time, GRP, xlab = "datetime", ylab = "Global_rective_power", type = "n")
 	lines(Time, GRP, col = "black")
 })
